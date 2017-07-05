f250cabb2d43c8f348e376872f3faabb2c4a797d
jiazai_wu <jiazai_wu@htc.com>
2017-07-05 14:30:17 +0800

    go for leetcode question 2
    go do not use while
    val := &structName{} to create struct*
    use nil not NULL

---
+++ leetCode/2.addTwoNumbers/main.go
@@ -0,0 +1,32 @@
+package main
+
+type ListNode struct {
+   Val int
+   Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    sum := 0;
+    head := &ListNode{};
+    node := &head;
+
+    for {
+        if l1 != nil {
+            sum += l1.Val;
+            l1 = l1.Next;
+        }
+
+        if l2 != nil {
+            sum += l2.Val;
+            l2 = l2.Next;
+        }
+        *node = &ListNode{Val: sum%10};
+        sum /= 10;
+        if l1 == nil && l2 == nil && sum == 0 {
+            break;
+        }
+		node = &(*node).Next;
+    }
+    
+    return head;
+}
